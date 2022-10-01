d2d0db7b1ce63468d1be9d8deba7caaa561a5cae
Kushagra Mittal <67522625+kshgr@users.noreply.github.com>
2022-10-01 21:27:19 +0100

    Update main.py

--- Part 5 - Commands/main.py
+++ Part 5 - Commands/main.py
@@ -2,7 +2,6 @@ import hikari
 import lightbulb
 from secret import TOKEN
 
-
 bot = lightbulb.BotApp( token=TOKEN,
                         intents=hikari.Intents.ALL_MESSAGES,
                         prefix="!",
@@ -41,15 +40,23 @@ async def ping(ctx: lightbulb.Context):
     # Send a message to the channel the command was used in
     await ctx.respond(f"Pong! Latency: {bot.heartbeat_latency*1000:.2f}ms")
 
-
+bot.run()
 
 '''
                 Command via Message Event
-@bot.listen(hikari.MessageCreateEvent)
-async def ping(event=hikari.MessageCreateEvent):
-    if event.is_human:
-        if event.content.strip() == "!ping":
-            await event.message.respond(f"Pong! Latency: {bot.heartbeat_latency*1000:.2f}ms")
-'''
 
-bot.run()
+bot = hikari.GatewayBot(token=TOKEN)
+
+# Register the event to the Bot
+@bot.listen(hikari.MessageCreateEvent)
+# Define the events's callback. The callback should take a single argument which will be
+# an instance of a subclass of hikari.Events
+async def ping(event=hikari.MessageCreateEvent):
+    # Create a condition to accept commands only from Human Users.
+    if event.is_human:
+        # Create a condition to read the content to check if command is called
+        if event.content.strip() == "!ping":
+            # Send a message to the channel the command was used in
+            await event.message.respond(f"Pong! Latency: {bot.heartbeat_latency*1000:.2f}ms")
+
+'''
